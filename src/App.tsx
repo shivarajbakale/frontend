cb47088937c777b42e4fc20f319abd8a79a7c73d
Shivaraj Bakale <shivaraj.bakale@apollo.io>
2025-01-24 17:27:53 +0530

    Added the changes for the tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import '@mantine/core/styles.css';
-
 import { MantineProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Router } from './Router';
 import { theme } from './theme';
 
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
-      <Router />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+        <Router />
+      </MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }

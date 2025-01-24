29e393dc03ed8ca5b686c80fe4312ef1b614dea1
Shivaraj Bakale <shivaraj.bakale@apollo.io>
2025-01-24 17:37:55 +0530

    Added the changes for the maintine setup

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,23 +1,33 @@
-import { Anchor, Text, Title } from '@mantine/core';
+import { Anchor, Button, Container, Text, Title } from '@mantine/core';
+import { useNavigate } from 'react-router';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
+  const navigate = useNavigate();
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+        <Text
+          inherit
+          variant="gradient"
+          component="span"
+          gradient={{ from: 'pink', to: 'yellow' }}
+        >
           Mantine
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Vite project includes a minimal setup, if you want to learn more on Mantine +
-        Vite integration follow{' '}
+        This starter Vite project includes a minimal setup, if you want to learn
+        more on Mantine + Vite integration follow{' '}
         <Anchor href="https://mantine.dev/guides/vite/" size="lg">
           this guide
         </Anchor>
         . To get started edit pages/Home.page.tsx file.
       </Text>
+      <Container>
+        <Button onClick={() => navigate('/homepage')}>Go to Homepage</Button>
+      </Container>
     </>
   );
 }
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,10 +1,12 @@
+import { Welcome } from '@/components/Welcome/Welcome';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './Home.page';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Welcome />} />
+      <Route path="/homepage" element={<HomePage />} />
     </Routes>
   );
 }

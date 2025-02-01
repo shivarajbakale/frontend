42726e81a6a03cd3cb64b1f6dee67e818f5ab30b
Shivaraj Bakale <shivaraj.bakale@apollo.io>
2025-02-01 12:18:54 +0530

    Added the changes for the client

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -4,4 +4,14 @@ const apiClient = axios.create({
   baseURL: "/api",
 });
 
+const apiClientStaging = axios.create({
+  baseURL: import.meta.env.STAGING_API_URL,
+});
+
+const apiClientProduction = axios.create({
+  baseURL: import.meta.env.PRODUCTION_API_URL,
+});
+
+export { apiClient, apiClientStaging, apiClientProduction };
+
 export default apiClient;

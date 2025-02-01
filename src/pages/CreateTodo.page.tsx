8832624c8fee9a22303a53272759937e7e8abe2a
Shivaraj Bakale <shivaraj.bakale@apollo.io>
2025-02-01 12:13:07 +0530

    Added the changes for the documentation

--- src/pages/CreateTodo.page.tsx
+++ src/pages/CreateTodo.page.tsx
@@ -1,16 +1,14 @@
 import { Container, Title } from "@mantine/core";
 import CreateTodoForm from "@/components/organisms/forms/CreateTodo";
 import { useNavigate } from "react-router-dom";
-import { useCreateTodo, useTodos } from "@/utils/queries/todo.queries";
+import { useCreateTodo } from "@/utils/queries/todo.queries";
 
 export const CreateTodoPage = () => {
   const { mutate: createTodo } = useCreateTodo();
-  const { refetch: refetchTodos } = useTodos();
   const navigate = useNavigate();
 
   const handleSubmit = async (values: any) => {
     await createTodo(values);
-    await refetchTodos();
     navigate("/todos");
   };
 
@@ -19,7 +17,7 @@ export const CreateTodoPage = () => {
       <Title order={2} className="text-center">
         Create New Todo
       </Title>
-      <Container>
+      <Container w="75%">
         <CreateTodoForm onSubmit={handleSubmit} />
       </Container>
     </Container>
